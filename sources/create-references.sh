e8e788f83125a05bf1a146e97059c07ad776c78a
Reshabh Sharma <reshabhsh@gmail.com>
2018-10-05 16:55:46 +0530

    Fix intendation with gg=G

--- sources/create-references.sh
+++ sources/create-references.sh
@@ -17,38 +17,38 @@ tar jxf /tmp/synfig-$DEFAULT_VERSION.tar.bz2 -C /tmp
 COMPONENTS="blend-methods converters layers"
 
 for COMPONENT in $COMPONENTS; do
-  pushd $COMPONENT
-    for dir in * ; do
-      if [ -d ${dir} ]; then
-        pushd $dir
+	pushd $COMPONENT
+	for dir in * ; do
+		if [ -d ${dir} ]; then
+			pushd $dir
 
-# Check if version file already exist
-          if [ -f "$dir.txt" ]; then
-            VERSION=`cat $dir.txt`
-          else
-            VERSION=$DEFAULT_VERSION
-          fi
+			# Check if version file already exist
+			if [ -f "$dir.txt" ]; then
+				VERSION=`cat $dir.txt`
+			else
+				VERSION=$DEFAULT_VERSION
+			fi
 
-# Check if the required version binaries already exist
-          if [ ! -d "/tmp/synfig-$VERSION" ]; then
-            wget "https://sourceforge.net/projects/synfig/files/releases/$VERSION/linux/synfigstudio-$VERSION.x86_64.tar.bz2/download" \ 
-	    	-O "/tmp/synfig-$DEFAULT_VERSION.tar.bz2"
+			# Check if the required version binaries already exist
+			if [ ! -d "/tmp/synfig-$VERSION" ]; then
+				wget "https://sourceforge.net/projects/synfig/files/releases/$VERSION/linux/synfigstudio-$VERSION.x86_64.tar.bz2/download" \ 
+				-O "/tmp/synfig-$DEFAULT_VERSION.tar.bz2"
 
-            mkdir -p /tmp/synfig-$VERSION
-            tar jxf /tmp/synfig-$VERSION.tar.bz2 -C /tmp
-          fi
-          pushd ../../../references
-            mkdir -p ./$COMPONENT/$dir
-          popd
-          CURRENT_DIR=`pwd`
-          for sample in * ; do
-# Renders every sif file present 
-            if [ "${sample##*.}" = "sif" ]; then
-              /tmp/synfig-$VERSION/bin/synfig -i $CURRENT_DIR/$sample -o $CURRENT_DIR/../../../references/$COMPONENT/$dir/"${sample%.*}".png
-            fi
-          done
-        popd
-      fi
-    done
-  popd 
+				mkdir -p /tmp/synfig-$VERSION
+				tar jxf /tmp/synfig-$VERSION.tar.bz2 -C /tmp
+			fi
+			pushd ../../../references
+			mkdir -p ./$COMPONENT/$dir
+			popd
+			CURRENT_DIR=`pwd`
+			for sample in * ; do
+				# Renders every sif file present 
+				if [ "${sample##*.}" = "sif" ]; then
+					/tmp/synfig-$VERSION/bin/synfig -i $CURRENT_DIR/$sample -o $CURRENT_DIR/../../../references/$COMPONENT/$dir/"${sample%.*}".png
+				fi
+			done
+			popd
+		fi
+	done
+	popd 
 done
