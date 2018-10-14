094dc35c257398612d69223902aa1a3853d2d411
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-10-14 20:24:52 +0700

    Debug deploy #4

--- deploy-reference.sh
+++ deploy-reference.sh
@@ -10,7 +10,7 @@ openssl aes-256-cbc -K $encrypted_8843021b8580_key -iv $encrypted_8843021b8580_i
 chmod 600 ./deploy_key
 eval `ssh-agent -s`
 cp ./deploy_key ~/.ssh/id_rsa
-ls ~/.ssh/
+ls ~/.ssh/ -al
 
 git config --global push.default simple
 git config user.name "Travis CI"
@@ -19,4 +19,4 @@ git config user.email "travis@travis-ci.org"
 git checkout "${TRAVIS_BRANCH}"
 git add --all
 git commit -m "Update reference png for commits: ${TRAVIS_COMMIT_RANGE}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]"
-git push "git@github.com:${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
+GIT_SSH_COMMAND='ssh -i ./deploy_key' git push "git@github.com:${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
