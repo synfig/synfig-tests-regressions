b60261b160da96133f0e4d493274fcc017e25e46
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-10-14 18:37:58 +0700

    Configure auto-deply using deploy key approach (https://gist.github.com/domenic/ec8b0fc8ab45f39403dd)

--- deploy-reference.sh
+++ deploy-reference.sh
@@ -6,6 +6,11 @@ set -e
 
 cd "${TRAVIS_BUILD_DIR}"
 
+openssl aes-256-cbc -K $encrypted_8843021b8580_key -iv $encrypted_8843021b8580_iv -in ./deploy_key.enc -out ./deploy_key -d
+chmod 600 ./deploy_key
+eval `ssh-agent -s`
+cp ./deploy_key ~/.ssh/id_rsa
+
 git config --global push.default simple
 git config user.name "Travis CI"
 git config user.email "travis@travis-ci.org"
@@ -13,4 +18,4 @@ git config user.email "travis@travis-ci.org"
 git checkout "${TRAVIS_BRANCH}"
 git add --all
 git commit -m "Update reference png for commits: ${TRAVIS_COMMIT_RANGE}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]"
-git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
+git push "git@github.com:${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
