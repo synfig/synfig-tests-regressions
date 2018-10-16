f6efb653b612afcb00639a8d5e456e30f065855f
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-10-16 14:52:45 +0700

    Debug deploy 12

--- deploy-reference.sh
+++ deploy-reference.sh
@@ -3,6 +3,7 @@
 # Push all png 
 
 set -e
+set -x
 
 cd "${TRAVIS_BUILD_DIR}"
 
@@ -19,4 +20,5 @@ git config user.email "travis@travis-ci.org"
 git checkout "${TRAVIS_BRANCH}"
 git add --all
 git commit -m "Update reference png for commits: ${TRAVIS_COMMIT_RANGE}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]"
+rm .git/hooks/pre-push # from https://github.com/git-lfs/git-lfs/issues/2291#issuecomment-305887405
 GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa -vvv" git push "git@github.com:${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
