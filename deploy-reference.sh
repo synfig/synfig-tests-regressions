cebbb42e71cd2bf4237cadca2f1826670b9ceb0e
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-10-17 18:47:57 +0700

    Test 6

--- deploy-reference.sh
+++ deploy-reference.sh
@@ -10,7 +10,7 @@ cd "${TRAVIS_BUILD_DIR}"
 openssl aes-256-cbc -K $encrypted_8843021b8580_key -iv $encrypted_8843021b8580_iv -in ./deploy_key.enc -out ./deploy_key -d
 chmod 600 ./deploy_key
 eval `ssh-agent -s`
-cp ./deploy_key $HOME/.ssh/id_rsa
+ssh-add deploy_key
 
 git config --global push.default simple
 git config user.name "Travis CI"
@@ -19,5 +19,4 @@ git config user.email "travis@travis-ci.org"
 git checkout "${TRAVIS_BRANCH}"
 git add --all
 git commit -m "Update reference png for commits: ${TRAVIS_COMMIT_RANGE}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]"
-rm .git/hooks/pre-push # from https://github.com/git-lfs/git-lfs/issues/2291#issuecomment-305887405
-GIT_SSH_COMMAND="ssh -i $HOME/.ssh/id_rsa" git push "git@github.com:${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
+git push "git@github.com:${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
