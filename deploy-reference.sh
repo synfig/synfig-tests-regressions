cd89a352b9784a4a295b8895e89f0f53f9a82198
Konstantin Dmitriev <ksee.zelgadis@gmail.com>
2018-10-18 21:00:22 +0700

    Failed to implement deploy key approach with GitLFS. Reverting to commit 81a0a992.

--- deploy-reference.sh
+++ deploy-reference.sh
@@ -3,14 +3,8 @@
 # Push all png 
 
 set -e
-set -x
 
 cd "${TRAVIS_BUILD_DIR}"
-
-openssl aes-256-cbc -K $encrypted_8843021b8580_key -iv $encrypted_8843021b8580_iv -in ./deploy_key.enc -out ./deploy_key -d
-chmod 600 ./deploy_key
-eval `ssh-agent -s`
-ssh-add deploy_key
 
 git config --global push.default simple
 git config user.name "Travis CI"
@@ -19,4 +13,4 @@ git config user.email "travis@travis-ci.org"
 git checkout "${TRAVIS_BRANCH}"
 git add --all
 git commit -m "Update reference png for commits: ${TRAVIS_COMMIT_RANGE}" -m "Travis build: ${TRAVIS_BUILD_NUMBER} [ci skip]"
-git push "git@github.com:${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
+git push "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" "${TRAVIS_BRANCH}"
