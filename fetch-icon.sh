c6c43f9b0ce7c39e4a14b282080946945314e3b5
Ankit Dwivedi <dwivedi.ankit21@gmail.com>
2018-12-17 20:27:51 +0530

    Added fetch-icon.sh

---
+++ fetch-icon.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+CURRENT_DIR=$(pwd)
+DIR=/tmp/images
+
+
+# create if /sources/icons/ does not exist
+echo "Checking if "$CURRENT_DIR"/sources/icons/ exist ..."
+if [ ! -d "$CURRENT_DIR"/sources/icons ]; then
+    mkdir "$CURRENT_DIR"/sources/icons
+    echo "Created /sources/icons/ ..."
+fi
+
+#check if svn is installed ,if not them prompt tp install
+
+if [[ -z $(which svn) ]]; then
+    echo "Subversion is not installed."
+    read -p "Do you wish to install this program?[Y/n]" yn
+    case $yn in
+        [Yy]* ) sudo apt-get install subversion;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+fi
+
+
+# delete if DIR=/tmp/images already exist
+
+if [ -d "$DIR" ]; then
+    rm -rf "$DIR"
+fi
+
+
+svn checkout https://github.com/synfig/synfig/trunk/synfig-studio/images /tmp/images/
+
+
+#svn checkout the images to DIR=/tmp/images
+svn checkout https://github.com/synfig/synfig/trunk/synfig-studio/images /tmp/images/
+
+
+#cd to the DIR and copt them to /sources/icons/
+cd "$DIR"
+for f in *.sif
+do 
+   cp -v "$f" "$CURRENT_DIR"/sources/icons/
+done
+
+
+
